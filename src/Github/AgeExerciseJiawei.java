586690cad3791b2d04ac02936a5c540c072c75e4
Jiawei Li <jiawei23000@student.hamk.fi>
2023-10-06 22:46:10 +0300

    First edition

---
+++ src/Github/AgeExerciseJiawei.java
@@ -0,0 +1,94 @@
+package Github;
+
+public class AgeExerciseJiawei {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int age = 100;
+		if (age > 0 && age < 18)
+		{
+			System.out.println("You are underage.");
+		}
+//		
+//		else
+//		{
+//			System.out.println("You are an adult.");
+//		}
+//			
+//		System.out.println("Press space to exit.");
+//			//2
+//		
+//			
+//		if (age > 0 && age < 18) 
+//		{ 
+//			System.out.println("You are underage."); 
+//		}
+//		
+//		else if (age >= 65) 
+//		{ 
+//			System.out.println("You are retired."); 
+//			} 
+//		
+//		else 
+//		{ 
+//			System.out.println("You are an adult."); 
+//			}	
+//			//3
+//		
+//		if (age > 0 && age < 18) 
+//		{ 
+//			System.out.println("You are underage."); 
+//			}
+//			
+//			
+//		if (age >= 15) 
+//		{
+//			System.out.println("You can drive a moped.");
+//			} 
+//		
+//		else if (age >= 65) 
+//		{ 
+//			System.out.println("You are retired.");
+//			} 
+//		
+//		else 
+//		{ 
+//			System.out.println("You are an adult."); 
+//			}
+//		//4
+		
+		else if (age == 18)
+		{
+			System.out.println("You can drive a car."); 
+		}
+		
+		else if (age == 10 || age == 20 || age ==30 || age ==40 || age ==50 || age ==60 || age ==70 || age ==80 || age ==90 || age ==100 || age ==110 || age ==120 )
+		{
+			System.out.println("Anniversary Party!");
+		}
+		
+		else if (age == 100)
+		{
+			System.out.println("Congratulations!\n Congratulations!\nCongratulations!"); 
+		}
+		
+		else if (age >= 40 && age <= 50)
+		{
+			System.out.println("Happy mid-life!"); 
+		}
+
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+	}
+
+}
