d92df0e2d73a77c13182cd39ab1a0f5269eab2ff
Jiawei Li <jiawei23000@student.hamk.fi>
2023-10-06 21:55:02 +0300

    Add comment and New printing

--- src/Github/Github01.java
+++ src/Github/Github01.java
@@ -5,6 +5,11 @@ public class Github01 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		//comparing two strings in selection stucture
+		
+		System.out.println("New printing!");
+		
+		
 	}
 
 }
